4a5d327a89c0cffda299046a1cb4ff167a726d50
luiseduardo85 <77890900+luiseduardo85@users.noreply.github.com>
2022-07-29 06:42:34 -0300

    Add files via upload

--- app.js
+++ app.js
@@ -2,9 +2,12 @@ const express = require("express");
 
 const bodyParser = require("body-parser");
 
+const date = require(__dirname + "/date.js");
+
 const app = express();
 
-var items = [];
+const items = [];
+const workItems = [];
 
 app.set("view engine", "ejs");
 
@@ -13,27 +16,30 @@ app.use(express.static("public"));
 
 app.get("/", function(req, res){
 
-    let today = new Date();
-
-    let options = {
-        weekday: "long",
-        day: "numeric",
-        month: "long",
-    };
-
-    let day = today.toLocaleDateString("pt-br", options);
-
-    res.render("list", {kindOfDay: day, newListItems: items});
+    let day = date.getDate();
+    res.render("list", {listTitle: day, newListItems: items});
 });
 
 app.post("/", function(req,res){
-    var item = req.body.newItem;
+    let item = req.body.newItem;
 
-     items.push(item);
+    if( req.body.list === "work"){
+        workItems.push(item);
+        res.redirect("/work")
+    } else{
+        items.push(item);
+        res.redirect("/");
+    }
    
-   res.redirect("/");
 })
 
+app.get("/work", function(req, res){
+    res.render("list", {listTitle: "work list", newListItems: workItems});
+});
+
+app.get("/about", function(req, res){
+    res.render("about");
+})
 
 app.listen(3000, function(){
     console.log("Server started on port 3000");
